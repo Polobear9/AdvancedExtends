cee8a565b975e8c1a39d6a1d9fe99355d5353a4e
Polobear9 <lieams1996@gmail.com>
2022-09-01 14:48:31 +0900

    Test Intellij Commit to Change the File.

--- src/Class1.java
+++ src/Class1.java
@@ -1,19 +1,22 @@
 public class Class1 extends ClassA
                     implements InterfaceA, InterfaceB, InterfaceC{
+    //new InterfaceA();
+    //Interface is can not be instanced
     @Override
     public void hoge(){
         System.out.println("hello");
     }
     //Can add a many Interface, and Also the Method can be double.
+    @Override
     public void hello(){
         System.out.println("hello World!");
     }
-
+    @Override
     public void hello_World(){
         System.out.println("hello World! Method!");
     }
 
-
+    @Override
     public void hello_Sky(){
         System.out.println("hello World! Sky Method!");
     }
